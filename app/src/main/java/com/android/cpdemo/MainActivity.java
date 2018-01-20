8769110337b6e5989a72cba1211a0edd42f64a98
Avinash Ranjan <avinash.ranjan21@gmail.com>
2018-01-20 20:41:02 +0530

    build version fixed & CRUD method added

--- app/src/main/java/com/android/cpdemo/MainActivity.java
+++ app/src/main/java/com/android/cpdemo/MainActivity.java
@@ -1,12 +1,16 @@
 package com.android.cpdemo;
 
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.android.cpdemo.data.NationContract;
 import com.android.cpdemo.data.NationDbHelper;
 
 
@@ -16,6 +20,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 	private Button btnInsert, btnUpdate, btnDelete, btnQueryRowById, btnDisplayAll;
 
 	private static final String TAG = MainActivity.class.getSimpleName();
+	private SQLiteDatabase sqLiteDatabase;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +28,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 		setContentView(R.layout.activity_main);
 
 		NationDbHelper nationDbHelper = new NationDbHelper(this);
-		SQLiteDatabase sqLiteDatabase = nationDbHelper.getWritableDatabase();
+		sqLiteDatabase = nationDbHelper.getWritableDatabase();
 
 		etCountry 		= (EditText) findViewById(R.id.etCountry);
 		etContinent 	= (EditText) findViewById(R.id.etContinent);
@@ -52,25 +57,148 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 		switch (v.getId()) {
 
 			case R.id.btnInsert:
-//				insert();
+				insert();
 				break;
 
 			case R.id.btnUpdate:
-//				update();
+				update();
 				break;
 
 			case R.id.btnDelete:
-//				delete();
+				delete();
 				break;
 
 			case R.id.btnQueryByID:
-//				queryRowById();
+				queryRowById();
 				break;
 
 			case R.id.btnDisplayAll:
-//				queryAndDisplayAll();
+				queryAndDisplayAll();
 				break;
 		}
+	}
+
+	private void insert() {
+
+		String countryName     = etCountry.getText().toString();
+		String continentName= etContinent.getText().toString();
+
+		ContentValues contentValues = new ContentValues();
+		contentValues.put(NationContract.NationEntry.COLUMN_COUNTRY, countryName);
+		contentValues.put(NationContract.NationEntry.COLUMN_CONTINENT, continentName);
+
+		long rowId = sqLiteDatabase.insert(NationContract.NationEntry.TABLE_NAME, null, contentValues);
+		Log.i(TAG, "Items inserted in table with row id: " + rowId);
+	}
+
+	private void update() {
+
+		String whereCountry = etWhereToUpdate.getText().toString();
+		String newContinent = etNewContinent.getText().toString();
+
+		String selection = NationContract.NationEntry.COLUMN_COUNTRY + " = ?";
+		String[] selectionArgs = { whereCountry };       // WHERE country = ? = Japan
+
+		ContentValues contentValues = new ContentValues();
+		contentValues.put(NationContract.NationEntry.COLUMN_CONTINENT, newContinent);
+
+		int rowsUpdated = sqLiteDatabase.update(NationContract.NationEntry.TABLE_NAME, contentValues, selection, selectionArgs);
+		Log.i(TAG, "Number of rows updated: " + rowsUpdated);
+	}
+
+	private void delete() {
+
+		String countryName = etWhereToDelete.getText().toString();
+
+		String selection = NationContract.NationEntry.COLUMN_COUNTRY + " = ? ";
+		String[] selectionArgs = { countryName };     // WHERE country = "Japan"
+
+		int rowsDeleted = sqLiteDatabase.delete(NationContract.NationEntry.TABLE_NAME, selection, selectionArgs);
+		Log.i(TAG, "Number of rows deleted: " + rowsDeleted);
+	}
+
+	private void queryRowById() {
+
+		String rowId = etQueryRowById.getText().toString();
+
+		String[] projection = {
+				NationContract.NationEntry._ID,
+				NationContract.NationEntry.COLUMN_COUNTRY,
+				NationContract.NationEntry.COLUMN_CONTINENT
+		};
+
+		// Filter results. Make these null if you want to query all rows
+		String selection = NationContract.NationEntry._ID + " = ? ";  // _id = ?
+		String[] selectionArgs = { rowId };             // Replace '?' by rowId in runtime    // _id = 5
+
+		String sortOrder = null;   // Ascending or Descending ...
+
+		Cursor cursor = sqLiteDatabase.query(NationContract.NationEntry.TABLE_NAME,    // The table name
+				projection,                 // The columns to return
+				selection,                  // Selection: WHERE clause OR the condition
+				selectionArgs,              // Selection Arguments for the WHERE clause
+				null,                       // don't group the rows
+				null,                       // don't filter by row groups
+				sortOrder);                // The sort order
+
+		if (cursor != null && cursor.moveToNext()) {
+
+			String str = "";
+
+			String[] columns = cursor.getColumnNames();
+			for (String column : columns) {
+				str += "\t" + cursor.getString(cursor.getColumnIndex(column));
+			}
+			str += "\n";
+
+			cursor.close();
+			Log.i(TAG, str);
+		}
+	}
+
+	private void queryAndDisplayAll() {
+
+		String[] projection = {
+				NationContract.NationEntry._ID,
+				NationContract.NationEntry.COLUMN_COUNTRY,
+				NationContract.NationEntry.COLUMN_CONTINENT
+		};
+
+		// Filter results. Make these null if you want to query all rows
+		String selection = null;
+		String[] selectionArgs = null;
+
+		String sortOrder = null;   // Ascending or Descending ...
+
+		Cursor cursor = sqLiteDatabase.query(NationContract.NationEntry.TABLE_NAME,    // The table name
+				projection,                 // The columns to return
+				selection,                  // Selection: WHERE clause OR the condition
+				selectionArgs,              // Selection Arguments for the WHERE clause
+				null,                       // don't group the rows
+				null,                       // don't filter by row groups
+				sortOrder);                // The sort order
+
+		if (cursor != null) {
+
+			String str = "";
+			while (cursor.moveToNext()) {  // Cursor iterates through all rows
+
+				String[] columns = cursor.getColumnNames();
+				for (String column : columns) {
+					str += "\t" + cursor.getString(cursor.getColumnIndex(column));
+				}
+				str += "\n";
+			}
+
+			cursor.close();
+			Log.i(TAG, str);
+		}
+	}
+
+	@Override
+	protected void onDestroy() {
+		sqLiteDatabase.close();  // Close Database Connection
+		super.onDestroy();
 	}
 
 }
